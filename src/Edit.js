5cded37a1c52154d62965e685a14b23156b21a38
vinutha-101 <nvinutha101@gmail.com>
2025-02-27 16:13:07 +0530

    Create Edit.js
    Added Edit.js placeholder to fix missing module error

---
+++ src/Edit.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Edit = () => {
+  return (
+    <div className="container mt-5">
+      <h2 className="text-center">Edit Page</h2>
+      <p>Coming Soon...</p>
+    </div>
+  );
+};
+
+export default Edit;
